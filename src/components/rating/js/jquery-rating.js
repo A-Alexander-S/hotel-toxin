cb31af2d620677143e8563a9340043e5c546cf69
Alexander <impoossible.777@gmail.com>
2021-10-17 21:01:41 +0400

    ui_kit pages formsElement: rating js - not finished

---
+++ src/components/rating/js/jquery-rating.js
@@ -0,0 +1,59 @@
+// let $ = require("jquery");
+// import "../../../../node_modules/rater-js/lib/rater-js.js"
+// import "rater-js/lib/style.css"
+// var rater = require("rater-js");
+// import "rater-js/index.js"
+
+
+
+jQuery(".rating-btn").html("Next Step...")
+
+// $(".rating").rate();
+
+//or for example
+// var options = {
+//   max_value: 6,
+//   step_size: 0.5,
+//   starSize: 32,
+  // element: document.querySelector("#rater"),
+  // rateCallback: function rateCallback(rating, done) {
+  //   this.setRating(rating);
+  //   done();
+  // }
+  // symbols: {
+  //   utf8_star: {
+  //     base: '\u2606',
+  //     hover: '\u2605',
+  //     selected: '\u2605',
+  //   },
+  // }
+// }
+// $(".rating").rate(options);
+
+// options = {
+//   symbols = {
+//     utf8_star: {
+//       base: '\u2606',
+//       hover: '\u2605',
+//       selected: '\u2605',
+//     },
+//     utf8_hexagon: {
+//       base: '\u2B21',
+//       hover: '\u2B22',
+//       selected: '\u2B22',
+//     },
+//     hearts: '♥',
+//     fontawesome_star: {
+//       base: '',
+//       hover: '',
+//       selected: '',
+//     },
+//     utf32_emoticons: {
+//       base: [0x1F625, 0x1F613, 0x1F612, 0x1F604],
+//       hover: [0x1F625, 0x1F613, 0x1F612, 0x1F604],
+//       selected: [0x1F625, 0x1F613, 0x1F612, 0x1F604],
+//     }
+//   },
+//   selected_symbol_type: 'utf32_emoticons',
+//   convert_to_utf8: true
+// }
