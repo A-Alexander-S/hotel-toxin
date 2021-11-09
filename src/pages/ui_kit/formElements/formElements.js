dcf35e373b1e8c32b58d8d98eab4c53dc981e290
Alexander <impoossible.777@gmail.com>
2021-11-09 20:02:53 +0400

    ui_kit formsElement: general mixin dropdown

--- src/pages/ui_kit/formElements/formElements.js
+++ src/pages/ui_kit/formElements/formElements.js
@@ -1,5 +1,5 @@
 import "./formElements.scss";
-import "../../../components/amount-guest/js/amount-guest.js";
+// import "../../../components/amount-guest/js/amount-guest.js";
 import "../../../components/like-button/js/like-button.js";
 import "../../../components/rating/js/rating.js";
 import "../../../components/range-slider/js/range-slider.js";
